614874997ee54639e714d5c9e7d0d3349c43be26
Clems-web <clement.servais1@gmail.com>
2021-05-18 14:43:04 +0200

    added left menu on user profile

--- View/profile.view.php
+++ View/profile.view.php
@@ -1,6 +1,10 @@
 <div id="profileContainer">
     <div id="profileSettings">
-
+        <ul>
+            <li>Change username</li>
+            <li>Change password</li>
+            <li>Change mail</li>
+        </ul>
     </div>
     <div id="profileContent">
 
