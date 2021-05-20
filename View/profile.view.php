fd6ee3d57d53ad0e25a678805e69aa262eaf1722
Clems-web <clement.servais1@gmail.com>
2021-05-20 11:44:19 +0200

    added user's recipe in their profile

--- View/profile.view.php
+++ View/profile.view.php
@@ -1,3 +1,11 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/Model/DB.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/Model/Entity/User.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/Model/Entity/Recette.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/Model/Manager/RecetteManager.php';
+
+?>
+
 <div id="profileContainer">
     <div id="profileSettings">
         <ul>
@@ -7,6 +15,16 @@
         </ul>
     </div>
     <div id="profileContent">
-
+        <?php
+        $recette = new RecetteManager();
+        $recetteAuthor = $recette->getByAuthor($_SESSION['user']);
+        foreach ($recetteAuthor as $recipe) {?>
+            <div class="recettes">
+                <span><?= $recipe->getTitle();?></span>
+                <p><?= $recipe->getContent();?></p>
+            </div>
+        <?php
+        }
+        ?>
     </div>
 </div>
