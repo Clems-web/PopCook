c9b29bbccb7b7a503a546a278c2abe0c73047129
Clems-web <clement.servais1@gmail.com>
2021-05-27 15:30:29 +0200

    added button that will get us recipe by theme

--- View/recipe.view.php
+++ View/recipe.view.php
@@ -1,4 +1,12 @@
 <div id="recipeContainer">
+    <div id="category">
+        <ul>
+            <li><button>Film</button></li>
+            <li><button>Série</button></li>
+            <li><button>Manga/Anime</button></li>
+            <li><button>Roman/Bande-dessinée/Comics</button></li>
+        </ul>
+    </div>
     <div id="searchbarRecipe">
         <input type="search" id="inputSearch" placeholder="ex : Naruto, Nouilles, etc">
         <button type="submit" id="buttonSearch">loupe</button>
